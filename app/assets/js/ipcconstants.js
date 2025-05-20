f3755b7cf525844134c8b2fe9218901bdcf8810a
Williams <williamseduardo93@gmail.com>
2025-05-20 18:40:29 -0300

    Se actualiza el Launcher en cuanto a Diseño e Imagenes

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'a35e94e0-0348-4448-91e7-aa1d8fef5548'
 // SEE NOTE ABOVE.
 
 
